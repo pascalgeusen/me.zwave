0e7eca9249bbd8d3c06aa828197f2f500c696f41
Martijn Suijlen <Martijn Suijlen>
2016-08-02 16:55:45 +0200

    Added configuration to supported device

--- drivers/054336Z/driver.js
+++ drivers/054336Z/driver.js
@@ -1,67 +1,158 @@
 "use strict";
 
-const path			= require('path');
-const ZwaveDriver	= require('homey-zwavedriver');
+const path = require('path');
+const ZwaveDriver = require('homey-zwavedriver');
 
 // http://www.pepper1.net/zwavedb/device/145
 
-module.exports = new ZwaveDriver( path.basename(__dirname), {
-	debug: false,
-	capabilities: {
-		'onoff': {
-			'command_class'				: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			'command_get'				: 'SWITCH_MULTILEVEL_GET',
-			'command_set'				: 'SWITCH_MULTILEVEL_SET',
-			'command_set_parser'		: function( value ){
-				return {
-					'Value': ( value > 0 ) ? 'on/enable' : 'off/disable',
-					'Dimming Duration': 1
-				}
-			},
-			'command_report'			: 'SWITCH_MULTILEVEL_REPORT',
-			'command_report_parser'		: function( report ){
-				if( typeof report['Value'] === 'string' ) {
-					return report['Value'] === 'on/enable';
-				} else {
-					return report['Value (Raw)'][0] > 0;
-				}
+module.exports = new ZwaveDriver(path.basename(__dirname), {
+		debug : false,
+		capabilities : {
+			'onoff' : {
+				'command_class' : 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+				'command_get' : 'SWITCH_MULTILEVEL_GET',
+				'command_set' : 'SWITCH_MULTILEVEL_SET',
+				'command_set_parser' : function (value) {
+					return {
+						'Value' : (value > 0) ? 'on/enable' : 'off/disable',
+						'Dimming Duration' : 1
+					}
+				},
+				'command_report' : 'SWITCH_MULTILEVEL_REPORT',
+				'command_report_parser' : function (report) {
+					if (typeof report['Value'] === 'string') {
+						return report['Value'] === 'on/enable';
+					} else {
+						return report['Value (Raw)'][0] > 0;
+					}
 
-			}
-		},
-		'dim': {
-			'command_class'				: 'COMMAND_CLASS_SWITCH_MULTILEVEL',
-			'command_get'				: 'SWITCH_MULTILEVEL_GET',
-			'command_set'				: 'SWITCH_MULTILEVEL_SET',
-			'command_set_parser'		: function( value ){
-				return {
-					'Value': value * 100,
-					'Dimming Duration': 1
 				}
 			},
-			'command_report'			: 'SWITCH_MULTILEVEL_REPORT',
-			'command_report_parser'		: function( report ){
-				if( typeof report['Value'] === 'string' ) {
-					return ( report['Value'] === 'on/enable' ) ? 1.0 : 0.0;
-				} else {
-					return report['Value (Raw)'][0] / 100;
+			'dim' : {
+				'command_class' : 'COMMAND_CLASS_SWITCH_MULTILEVEL',
+				'command_get' : 'SWITCH_MULTILEVEL_GET',
+				'command_set' : 'SWITCH_MULTILEVEL_SET',
+				'command_set_parser' : function (value) {
+					return {
+						'Value' : value * 100,
+						'Dimming Duration' : 1
+					}
+				},
+				'command_report' : 'SWITCH_MULTILEVEL_REPORT',
+				'command_report_parser' : function (report) {
+					if (typeof report['Value'] === 'string') {
+						return (report['Value'] === 'on/enable') ? 1.0 : 0.0;
+					} else {
+						return report['Value (Raw)'][0] / 100;
+					}
 				}
 			}
+		},
+		settings : {
+			"led_mode" : {
+				"index" : 1,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"auto_off" : {
+				"index" : 2,
+				"size" : 2,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"rf_off_command" : {
+				"index" : 3,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"start_level" : {
+				"index" : 4,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"dimming_speed" : {
+				"index" : 5,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"dimming_long_speed" : {
+				"index" : 6,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"max_light_level_on_click" : {
+				"index" : 7,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"baby_dimming_time" : {
+				"index" : 8,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"target_dimming_level" : {
+				"index" : 9,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"click_timeout" : {
+				"index" : 10,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"invert_buttons" : {
+				"index" : 11,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"switch_by_buttons" : {
+				"index" : 12,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"action_on_single_press_or_hold" : {
+				"index" : 13,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"action_on_double_press_or_hold" : {
+				"index" : 14,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
+			"switch_all_commands" : {
+				"index" : 15,
+				"size" : 1,
+				"parser" : function (input) {
+					return new Buffer([parseInt(input)]);
+				}
+			},
 		}
-	},
-	settings: {
-		"switch_type": {
-			"index": 14,
-			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
-		},
-		"scene_activation": {
-			"index": 41,
-			"size": 1,
-			"parser": function( input ) {
-				return new Buffer([ parseInt(input) ]);
-			}
-		},
-	}
-})
+	})
